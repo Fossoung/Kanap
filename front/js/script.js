6c50d7d775bb2679238269adfc9ec546c90121b0
fossoung <fossoung@gmail.com>
2022-05-26 12:28:36 +0200

    commit update

--- front/js/script.js
+++ front/js/script.js
@@ -17,15 +17,11 @@ fetch("http://localhost:3000/api/products")
   });
 
 // fonction d'affichage des produits de l'api sur la page index
-
 function lesKanaps(index) {
   // déclaration de variable de la zone d'article
   let zoneArticle = document.querySelector("#items");
   // boucle pour chaque indice(nommé 'article') dans index
   for (let article of index) {
-    /* création et ajout des zones d'articles, insertion de l'adresse produit via chemin produit + paramètres(son id);
-    la page index est http://127.0.0.1:5500/front/html/index.html donc la page du produit sera http://127.0.0.1:5500/front/html/product.html 
-    (d'ou le ./product.html) pour rajouter son paramètre on met ? puis la clé (ici _id) associé (=) à sa valeur dynamique ${article._id} */
     zoneArticle.innerHTML += `<a href="./product.html?_id=${article._id}">
     <article>
       <img src="${article.imageUrl}" alt="${article.altTxt}">
